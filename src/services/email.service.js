e8353f5b8d78ee4d57a28d1f2975b4bc80c7f95b
ndmanh1302 <nguyenducmanh13021995@gmail.com>
2022-04-26 10:18:01 +0700

    Forgot password

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -32,7 +32,7 @@ const sendEmail = async (to, subject, text) => {
 const sendResetPasswordEmail = async (to, token) => {
   const subject = 'Reset password';
   // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
+  const resetPasswordUrl = `http://localhost:3000/password_resets/${token}/edit?email=${to}`;
   const text = `Dear user,
 To reset your password, click on this link: ${resetPasswordUrl}
 If you did not request any password resets, then ignore this email.`;
